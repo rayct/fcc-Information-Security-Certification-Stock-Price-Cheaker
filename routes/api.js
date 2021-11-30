f6da904d645da38ac695d89ce83c0ec7fbd908cb
rayct <rayct@icloud.com>
2021-11-30 18:38:14 +0000

    Added npm mongodb and mongoose

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,15 @@
 'use strict';
 
+
+let expect = require('chai').expect;
+let mongodb = require('mongodb')
+let mongoose = require('mongoose')
+
 module.exports = function (app) {
 
   app.route('/api/stock-prices')
-    .get(function (req, res){
-      
+    .get(function (req, res) {
+
     });
-    
+
 };
