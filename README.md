1f1b879a335e5d0e92b8d9e9c70b0290e2aedc03
rayct <rayct@icloud.com>
2021-12-03 01:17:34 +0000

    Get Api Stock Price

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # [Stock Price Checker](https://freecodecamp.org/learn/information-security/information-security-projects/stock-price-checker)
-Raymond C Turner
+Raymond Turner
+
+
+1.SET NODE_ENV to test without quotes and set DB to your MongoDB connection string
+2.Complete the project in routes/api.js or by creating a handler/controller
+3.You will add any security features to server.js
+4.You will create all of the functional tests in tests/2_functional-tests.js
--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,9 @@
 'use strict';
-
-let expect = require('chai').expect;
 let mongodb = require('mongodb')
 let mongoose = require('mongoose')
-
 module.exports = function (app) {
 
-  let uri = 'mongodb+srv://rayct:' + process.env.PWD + '@freecodecamp.kpipn.mongodb.net/stock_price_checker?retryWrites=true&w=majority'
+  let uri = 'mongodb+srv://rayct:' + process.env.PWD + '@cluster0.kpipn.mongodb.net/stock_price_checker?retryWrites=true&w=majority'
 
   mongoose.connect(uri, {
     useNewUrlParser: true,
@@ -24,7 +21,8 @@ module.exports = function (app) {
     },
     ips: [String]
   })
-  let Stock = mongoose.model('Stock', stockSchema)
+  let stock = mongoose.model('stock', stockSchema)
+
 
   app.route('/api/stock-prices')
     .get(function (req, res) {
@@ -42,22 +40,39 @@ module.exports = function (app) {
 
       /* Find/Update Stock Document */
       let findOrUpdateStock = (stockName, documentUpdate, nextStep) => {
-
+        Stock.findOneAndUpdate({
+            name: stockName
+          },
+          documentUpdate, {
+            new: true,
+            upsert: true
+          },
+          (error, stockDocument) => {
+            if (error) {
+              console.log(error)
+            } else if (!error && stockDocument) {
+              if (twoStocks === false) {
+                return nextStep(stockDocument, processOneStock)
+              }
+            }
+          }
+        )
       }
 
       /* Like Stock */
       let likeStock = (stockName, nextStep) => {
-
+        nextStep(stockDocument, outputResponse)
       }
 
       /* Get Price */
       let getPrice = (stockDocument, nextStep) => {
-
+        nextStep(stockDocument, outputResponse)
       }
 
       /* Build Response for 1 Stock */
       let processOneStock = (stockDocument, nextStep) => {
-
+        responseObject['stockData']['stock'] = stockDocument['name']
+        nextStep()
       }
 
       let stocks = []
@@ -69,17 +84,22 @@ module.exports = function (app) {
       /* Process Input*/
       if (typeof (req.query.stock) === 'string') {
         /* One Stock */
-
+        let stockName = req.query.stock
+        let documentUpdate = {}
+        findOrUpdateStock(stockName, documentUpdate, getPrice)
 
       } else if (Array.isArray(req.query.stock)) {
         twoStocks = true
         /* Stock 1 */
 
-
         /* Stock 2 */
 
-
       }
+    });
+
+  app.route('/api/stock-prices')
+    .get(function (req, res) {
+
     });
 
 };
